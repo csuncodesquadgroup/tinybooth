c4b7404c2d9d9f50ec3010f3d745bf38c252a3a4
Camrynn Dilley <dorkycam@Camrynns-MacBook-Pro.local>
2020-06-02 17:24:18 -0700

    initial

---
+++ tinybooth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tinybooth
+//
+//  Created by Camrynn Dilley on 6/2/20.
+//  Copyright © 2020 codesquad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
