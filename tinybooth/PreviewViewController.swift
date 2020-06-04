2427641d15d96c4633d7eca809a1bf2b0483ebfd
Camrynn Dilley <dorkycam@Camrynns-MacBook-Pro.local>
2020-06-04 15:10:58 -0700

    added view controller and previewViewController - tapping shutter button takes you to the preview screen and preview screen has buttons in place but those buttons are not yet operable

---
+++ tinybooth/PreviewViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PreviewViewController.swift
+//  tinybooth
+//
+//  Created by Camrynn Dilley on 6/4/20.
+//  Copyright © 2020 codesquad. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Linked to the "Redo" button on Preview screen of Main.storyboard
+    @IBAction func cancelButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    // Linked to the "Print" button on Preview screen of Main.storyboard
+    @IBAction func printButton_TouchUpInside(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tinybooth/ViewController.swift
+++ tinybooth/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // Linked to the camera/shutter button on maine view controller of Main.storyboard (like the initial screen you see when you open up the app
+    @IBAction func cameraButton_TouchUpInside(_ sender: Any) {
+        
+        // this opens up the PreviewVieweController when the camera/shutter button is tapped
+        performSegue(withIdentifier: "showPhotoSegue", sender: nil)
+    }
+    
 }
 
