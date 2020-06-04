a4fc7bb0dceb65fca2d5f971973387117c995b41
Camrynn Dilley <dorkycam@Camrynns-MacBook-Pro.local>
2020-06-04 15:24:13 -0700

    adding functions for capturing photo/using  cameera (more like setting it up)

--- tinybooth/ViewController.swift
+++ tinybooth/ViewController.swift
@@ -13,7 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Functions for capturing image
+        setupCaptureSession()
+        setupDevice()
+        setupInputOutput()
+        setupPrevieLayer()
+        startRunningCaptureSession()
     }
+    
+    // setting up capture session
+    func setupCaptureSession() {
+        
+    }
+    
+    // configuring necesary capture devices (camera)
+    func setupDevice() {
+        
+    }
+    
+    // creating input using capture devices
+    func setupInputOutput() {
+        
+    }
+    
+    // configuring photo output object to process captured images
+    func setupPrevieLayer() {
+        
+    }
+    
+    // start running when we have finished configuration
+    func startRunningCaptureSession() {
+        
+    }
+    
     // Linked to the camera/shutter button on maine view controller of Main.storyboard (like the initial screen you see when you open up the app
     @IBAction func cameraButton_TouchUpInside(_ sender: Any) {
         
