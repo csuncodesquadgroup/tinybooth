220ee053a1c6eb2da0122267695647a58c008ef8
Camrynn Dilley <dorkycam@gmail.com>
2020-06-18 18:52:57 -0700

    added contact info, working on adding link

--- HelpViewController.swift
+++ HelpViewController.swift
@@ -9,28 +9,25 @@
 import UIKit
 
 class HelpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var contactTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+   
         // Do any additional setup after loading the view.
     }
 
     @IBAction func exitButton_TouchUpInside(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-    }
+    }    
     
-    
-    
+    func contactTextLink() {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
+
+
+
