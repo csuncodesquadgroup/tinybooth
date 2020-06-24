c2cbcaff0d64099d15fbc0dc01770462403aac23
Camrynn Dilley <dorkycam@gmail.com>
2020-06-23 17:22:17 -0700

    added in the link now we are gucci

--- HelpViewController.swift
+++ HelpViewController.swift
@@ -8,26 +8,47 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIViewController, UITextViewDelegate {
     
     
-    @IBOutlet weak var contactTextLabel: UILabel!
+    @IBOutlet weak var contactTextView: UITextView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        updateTextView()
         // Do any additional setup after loading the view.
+
     }
 
+    func updateTextView() {
+        
+        let path = "https://github.com/csuncodesquadgroup/tinybooth"
+        let text = contactTextView.text ?? ""
+        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "github")
+        let font = contactTextView.font
+        let color = contactTextView.textColor
+        contactTextView.attributedText = attributedString
+        contactTextView.font = font
+        contactTextView.textColor = color
+    }
+    
     @IBAction func exitButton_TouchUpInside(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-    }    
-    
-    func contactTextLink() {
-
     }
 
 }
 
-
-
+extension NSAttributedString {
+    
+    static func makeHyperLink(for path: String, in string: String, as substring: String) -> NSAttributedString {
+        
+        let nsString = NSString(string: string)
+        let substringRange = nsString.range(of: substring)
+        let attributedString = NSMutableAttributedString(string: string)
+        attributedString.addAttribute(.link, value: path, range: substringRange)
+        return attributedString
+        
+    }
+    
+}
